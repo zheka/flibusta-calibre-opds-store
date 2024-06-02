bb87b8b3b3ea36bca1245a3219b716eb2ef70efe
zheka <evgeny.eltsin@gmail.com>
2024-06-02 13:44:01 +0200

    Refactor + support coollib

--- src/flibusta.py
+++ src/flibusta.py
@@ -18,10 +18,45 @@ from calibre.utils.opensearch.description import Description
 from calibre.utils.opensearch.query import Query
 from calibre.gui2.store.search_result import SearchResult
 
+class _Catalog:
+
+    def __init__(self, **args):
+        self._name = args.get('name')
+        self._web_url = args.get('web_url')
+        self._open_search_url = args.get('open_search_url')
+        self._search_url_template = args.get('search_url_template')
+
+    def name(self):
+        return self._name
+
+    def web_url(self):
+        return self._web_url
+
+    def link(self, href):
+        if href.startswith('/'):
+            return self._web_url + href
+        return href
+
+    def search_url_template(self):
+        if not self._search_url_template:
+            d = Description(self._open_search_url)
+            self._search_url_template = d.get_best_template()
+            print('{name} search url template: {url}'.format(name=self._name, url=self._search_url_template))
+        return self._search_url_template
+
+
 class FlibustaStore(StorePlugin):
 
-    open_search_url = 'https://flibusta.site/opds-opensearch.xml'
-    web_url = 'https://flibusta.site'
+    my_catalog = _Catalog(
+        name='Flibusta',
+        web_url='https://flibusta.is',
+        open_search_url='https://flibusta.is/opds-opensearch.xml')
+
+    #my_catalog = _Catalog(
+    #    name='Coollib',
+    #    web_url='https://coollib.net',
+    #    open_search_url='https://coollib.cc/opds-opensearch.xml',
+    #    search_url_template='http://coollib.net/opds/search?searchTerm={searchTerms}&searchType=books&pageNumber={startPage?}')
 
     def open(self, parent=None, detail_item=None, external=False):
         if not hasattr(self, 'web_url'):
@@ -36,32 +71,32 @@ class FlibustaStore(StorePlugin):
             d.exec()
 
     def search(self, query, max_results=10, timeout=60):
-        if not getattr(self, 'open_search_url', None):
-            return
+        yield from FlibustaStore.open_search(FlibustaStore.my_catalog, query, max_results, timeout)
 
-        yield from FlibustaStore.open_search(self.open_search_url, query, max_results, timeout)
-
-    def open_search(url, query, max_results, timeout):
-        description = Description(url)
-        url_template = description.get_best_template()
+    def open_search(catalog, query, max_results, timeout):
+        url_template = catalog.search_url_template()
         if not url_template:
             return
-        oquery = Query(url_template)
 
         # set up initial values
+        oquery = Query(url_template)
         oquery.searchTerms = query
         oquery.count = max_results
         url = oquery.url()
 
+        print('{name} search: {url}'.format(name=catalog.name(), url=url))
+
         counter = max_results
         br = browser()
         with closing(br.open(url, timeout=timeout)) as f:
-            doc = safe_xml_fromstring(f.read())
+            content = f.read()
+            doc = safe_xml_fromstring(content)
+
             for data in doc.xpath('//*[local-name() = "entry"]'):
                 if counter <= 0:
                     break
-
                 counter -= 1
+
                 s = SearchResult()
                 s.detail_item = ''.join(data.xpath('./*[local-name() = "id"]/text()')).strip()
 
@@ -72,20 +107,20 @@ class FlibustaStore(StorePlugin):
 
                     if rel and href and type:
                         if 'http://opds-spec.org/thumbnail' in rel:
-                            s.cover_url = href
+                            s.cover_url = catalog.link(href)
                         elif 'http://opds-spec.org/image/thumbnail' in rel:
-                            s.cover_url = href
+                            s.cover_url = catalog.link(href)
                         elif 'http://opds-spec.org/acquisition/buy' in rel:
-                            s.detail_item = href
+                            s.detail_item = catalog.link(href)
                         elif 'http://opds-spec.org/acquisition/sample' in rel:
                             pass
                         elif 'alternate' in rel:
-                            s.detail_item = FlibustaStore.web_url + href
+                            s.detail_item = catalog.link(href)
                         elif 'http://opds-spec.org/acquisition' in rel:
                             if type:
                                 ext = FlibustaStore.custom_guess_extension(type)
                                 if ext:
-                                    s.downloads[ext] = FlibustaStore.web_url + href
+                                    s.downloads[ext] = catalog.link(href)
 
                 s.formats = ', '.join(s.downloads.keys()).strip()
 
@@ -93,6 +128,10 @@ class FlibustaStore(StorePlugin):
                 s.author = ', '.join(data.xpath('./*[local-name() = "author"]//*[local-name() = "name"]//text()')).strip()
                 s.price = '$0.00'
                 s.drm = SearchResult.DRM_UNLOCKED
+
+                print('  {author} - {title}'.format(author=s.author, title=s.title))
+                for ext, url in s.downloads.items():
+                    print('    {ext}: {url}'.format(ext=ext, url=url))
 
                 yield s
 
@@ -104,5 +143,7 @@ class FlibustaStore(StorePlugin):
             return 'FB2'
         elif 'application/epub' in type:
             return 'EPUB'
+        elif 'application/x-mobipocket-ebook' in type:
+            return 'MOBI'
         else:
             return None
